f55989c0f5d3b5f716509d983561811a14556627
Krishna <Krishnasikheriya001@gmail.com>
2025-02-07 19:27:43 +0530

    changes made

--- app.js
+++ app.js
@@ -2,160 +2,114 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
-import { getDate, getDay } from './date.js';
-import mongoose from 'mongoose';
-import { Item , List} from "./models/item.js";
-import _ from 'lodash'
+import { getDate, getDay } from "./date.js";
+import mongoose from "mongoose";
+import { Item, List } from "./models/item.js";
+import _ from "lodash";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-// const items = ["Buy Food", "Cook Food", "Eat Food"]; 
-// const workItems = [];
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-//----------------------------------------------------------------------------------------
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-const item1 = new Item ({
-  name : "Do Laundry"
-});
-const item2 = new Item ({
-  name : "Do Homework"
-});
-const item3 = new Item ({
-  name : "Have Lunch"
-});
+const item1 = new Item({ name: "Do Laundry" });
+const item2 = new Item({ name: "Do Homework" });
+const item3 = new Item({ name: "Have Lunch" });
 
 const defaultItems = [item1, item2, item3];
 
-
-
 const day = getDay();
 
-
 app.get("/", (req, res) => {
-
-  //console.log(day);
-
   Item.find({})
-  .then((foundItems)=>{
-
-    if(foundItems.length == 0){
-      Item.insertMany(defaultItems)
-      // .then(()=>console.log("successfully Added!"))
-      .catch((err)=>console.log("Failed to add", err));
-      
-      res.redirect("/");
-    } else {
-      res.render("list", { listTitle: day , newListItems : foundItems});
-    }
-  })
-  .catch((err)=>console.log("Failed to fetch!", err));
+    .then((foundItems) => {
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems).catch((err) =>
+          console.log("Failed to add", err)
+        );
+        res.redirect("/");
+      } else {
+        res.render("list", { listTitle: day, newListItems: foundItems });
+      }
+    })
+    .catch((err) => console.log("Failed to fetch!", err));
 });
 
-
 app.post("/", (req, res) => {
-
   const itemName = req.body.newItem;
   const listName = req.body.list;
 
-  const item = new Item({
-    name: itemName
-  });
+  const item = new Item({ name: itemName });
 
-  if(listName === day){
-    item.save()
-    .then(()=>{ res.redirect("/") })
-    .catch(err => console.log("Error saving item:", err));
+  if (listName === day) {
+    item
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((err) => console.log("Error saving item:", err));
   } else {
-    List.findOne({name : listName})
-    .then((foundList)=>{
-      foundList.items.push(item);
-      foundList.save()
-      .then(()=>{
-        res.redirect("/" + listName)
+    List.findOne({ name: listName })
+      .then((foundList) => {
+        foundList.items.push(item);
+        return foundList.save();
       })
-    })
-    .catch((err)=>console.log("Failed to load the webpage",err));
+      .then(() => res.redirect("/" + listName))
+      .catch((err) => console.log("Failed to load the webpage", err));
   }
-})
+});
 
-app.post("/delete", function(req, res){
-
+app.post("/delete", (req, res) => {
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
-  //console.log(checkedItemId);
 
-  if(listName == day){
+  if (listName === day) {
     Item.findByIdAndDelete(checkedItemId)
-    .then(()=>{ 
-      //console.log("Item deleted")
-      res.redirect("/")
-    })
-    .catch((err)=>console.log("Failed to delete the item", err));
+      .then(() => res.redirect("/"))
+      .catch((err) => console.log("Failed to delete the item", err));
   } else {
     List.findOneAndUpdate(
       { name: listName },
       { $pull: { items: { _id: checkedItemId } } },
       { new: true }
     )
-    .then(updatedList => {
-      if (updatedList) {
-       // console.log("Item removed successfully:", updatedList);
-       res.redirect("/"+listName);
-
-      } else {
-        console.log("List not found!");
-      }
-    })
-    .catch(err => console.error("Error removing item:", err));    
+      .then((updatedList) => {
+        if (updatedList) {
+          res.redirect("/" + listName);
+        } else {
+          console.log("List not found!");
+        }
+      })
+      .catch((err) => console.error("Error removing item:", err));
   }
-})
+});
 
-app.get("/:customListName", function(req, res) {
+app.get("/:customListName", (req, res) => {
   let customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName })
     .then((foundList) => {
       if (!foundList) {
-        // List doesn't exist, create a new one (empty list)
-        const list = new List({
-          name: customListName,
-          items: [] // Empty list instead of default items
-        });
-
-        list.save().then(() => {
-          //console.log(`Created new empty list: ${customListName}`);
-          res.redirect("/" + customListName);
-        }).catch(err => console.log("Error saving new list:", err));
-
+        const list = new List({ name: customListName, items: [] });
+        return list.save().then(() => res.redirect("/" + customListName));
       } else {
-        // List exists, render it even if it's empty
-        //console.log(`Using existing list: ${customListName}`);
-        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
       }
     })
-    .catch(err => console.log("Error fetching list:", err));
+    .catch((err) => console.log("Error fetching list:", err));
 });
 
-
-
-app.get("/about", (req, res)=>{
+app.get("/about", (req, res) => {
   res.render("about");
-})
-
-
-app.listen(3000, function () {
-  console.log("Server is running on port 3000");
 });
 
-
-// Info  
-// variable created inside the if ,else, while, for loops statements
-// var : global(can be accessed from anywhere)
-// let, const : local (only access inside the if statement)
+// ✅ Export app instead of running it (Vercel handles it)
+export default app;
